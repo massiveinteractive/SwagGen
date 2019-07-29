fb023ccb2a3b2170e113d95a0096f64821d253ca
Robert Janik <robert.janik@guardedkey.com>
2019-07-29 13:44:52 +0200

    Revert.

--- Sources/SwagGen/main.swift
+++ Sources/SwagGen/main.swift
@@ -4,6 +4,6 @@ import SwagGenKit
 import SwiftCLI
 
 let version = "3.0.2"
-let generateCommand = Command()
+let generateCommand = GenerateCommand()
 let cli = CLI(name: "swaggen", version: version, description: "Swagger code generator", commands: [generateCommand])
 cli.goAndExit()
